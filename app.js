dd4cc3151d45b39e51cfbc3d41f002a3570176ea
Will Schaller <willschaller@gmail.com>
2018-07-12 17:36:16 +0200

    app.js comments

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// --- Dependencies ---//
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -8,30 +9,31 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
+// --- Instantiations ---//
 const app = express();
 
+// --- Configurations ---//
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// --- Middleware ---//
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// --- Routes ---//
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // -- 404 and error handler
-
-// NOTE: requires a views/not-found.ejs template
 app.use((req, res, next) => {
   res.status(404);
   res.render('not-found');
 });
 
-// NOTE: requires a views/error.ejs template
 app.use((err, req, res, next) => {
   // always log the error
   console.error('ERROR', req.method, req.path, err);

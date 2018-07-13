67ed509b6142d8ebe9663c81f1186c27bbff138e
Javier Romero <romeropaolini@gmail.com>
2018-07-13 16:50:33 +0200

    reformat listing model

--- models/listing.js
+++ models/listing.js
@@ -2,17 +2,38 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const ObjectId = Schema.Types.ObjectId;
+
+const genres = ['Classical', 'Blues', 'Rock', 'Pop', 'Hip-hop', 'Reggae', 'Jazz', 'Country', 'World', 'Electronic', 'Metal', 'Indie', 'R&B', 'Latin'];
 
 const listingSchema = new Schema({
-  title: { type: String, required: true },
-  url: { type: String, required: true },
-  image: { type: String },
-  artist: { type: String, required: true },
-  genres: [{ type: String, enum: ['Classical', 'Blues', 'Rock', 'Pop', 'Hip-hop', 'Reggae', 'Jazz', 'Country', 'World', 'Electronic', 'Metal', 'Indie', 'R&B', 'Latin'] }],
-  year: { type: Number },
-  duration: { type: Number },
-  location: { type: String }
+  title: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String
+  },
+  artist: {
+    type: String,
+    required: true
+  },
+  genres: [{
+    type: String,
+    enum: genres
+  }],
+  year: {
+    type: Number
+  },
+  duration: {
+    type: Number
+  },
+  location: {
+    type: String
+  }
 });
 
 const Listing = mongoose.model('Listing', listingSchema);

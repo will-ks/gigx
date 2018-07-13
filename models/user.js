e960a24a30f6b01f5552c57c5271903ad3274770
Javier Romero <romeropaolini@gmail.com>
2018-07-13 15:58:50 +0200

    fixed identation

--- models/user.js
+++ models/user.js
@@ -2,12 +2,21 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const ObjectId = Schema.Types.ObjectId;
 
 const userSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  avatar: { type: String, default: '/images/avatar.svg' }
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    default: '/images/avatar.svg'
+  }
 });
 
 const User = mongoose.model('User', userSchema);
--- routes/users/auth.js
+++ routes/users/auth.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // --- Dependencies --- //
 const express = require('express');
 const router = express.Router();

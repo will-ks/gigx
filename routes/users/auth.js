365ff95d8e62002d823871e6f7a3fde91c1ab314
Will Schaller <willschaller@gmail.com>
2018-07-13 10:55:02 +0200

    Fix signup routes

--- routes/users/auth.js
+++ routes/users/auth.js
@@ -11,22 +11,26 @@ const saltRounds = 10;
 
 // --- Routes --- //
 // Sign up
-router.get('/signup', (req, res, next) => {
+router.get('/', (req, res, next) => {
+  res.render('users/auth');
+});
+
+router.post('/signup', (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   // Check if user is already logged in
-  if (req.session.currentUser) {
-    return res.redirect('/');
-  }
+  // if (req.session.currentUser) {
+  //   return res.redirect('/');
+  // }
   // Check that a email and password have been provided
   if (!email || !password) {
     // Flash an error
-    return res.redirect('/signup');
+    return res.redirect('/users/auth');
   }
   // Check that email is a valid email
   if (!validator.isEmail(email)) {
     // Flash an error
-    return res.redirect('/signup');
+    return res.redirect('/users/auth');
   }
 
   // Check if email already exists in database
@@ -34,7 +38,7 @@ router.get('/signup', (req, res, next) => {
     .then(user => {
       if (user) {
         // Flash an error
-        return res.redirect('/signup');
+        return res.redirect('/users/auth');
       }
 
       // Create the user in the database

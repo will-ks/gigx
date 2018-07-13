f84fd7bde306436c22e320e3d8853da85f7ddbe2
Javier Romero <romeropaolini@gmail.com>
2018-07-13 16:51:43 +0200

    added listings routes

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const flash = require('connect-flash');
 
 const index = require('./routes/index');
 const auth = require('./routes/users/auth');
+const list = require('./routes/listings/list');
 
 // --- Instantiations --- //
 const app = express();
@@ -63,6 +64,7 @@ app.use((req, res, next) => {
 // --- Routes --- //
 app.use('/', index);
 app.use('/users/auth', auth);
+app.use('/listings/list', list);
 
 // -- 404 and error handler
 app.use((req, res, next) => {
---
+++ routes/listings/list.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// --- Dependencies --- //
+const express = require('express');
+const router = express.Router();
+
+// const Listing = require('../../models/listingModel');
+
+// --- Routes --- //
+router.get('/', (req, res, next) => {
+  res.render('/listings');
+});
+
+module.exports = router;

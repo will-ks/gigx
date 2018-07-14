affbe095a38357a33949ae66c962a084ebc40e56
Will Schaller <willschaller@gmail.com>
2018-07-14 15:56:51 +0200

    Added view listing route

--- routes/listings/index.js
+++ routes/listings/index.js
@@ -4,11 +4,24 @@
 const express = require('express');
 const router = express.Router();
 
-// const Listing = require('../../models/listingModel');
+const Listing = require('../../models/listing');
 
 // --- Routes --- //
 router.get('/', (req, res, next) => {
-  res.render('listings/');
+  res.render('listings');
+});
+
+router.get('/view/:id', (req, res, next) => {
+  const listingId = req.params.id;
+  // TODO: Check that id is valid ObjectID, redirect if not
+  Listing.findById(listingId)
+    .then(listing => {
+      if (!listing) {
+        res.redirect('/listings');
+      }
+      res.render('listings/view', listing);
+    })
+    .catch(next);
 });
 
 module.exports = router;

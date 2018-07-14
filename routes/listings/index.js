641b22c994341d4607a8199de81e90ce31a01e01
Will Schaller <willschaller@gmail.com>
2018-07-14 18:39:11 +0200

    Add POST route for adding a listing

--- routes/listings/index.js
+++ routes/listings/index.js
@@ -3,6 +3,7 @@
 // --- Dependencies --- //
 const express = require('express');
 const router = express.Router();
+const validator = require('validator');
 
 const Listing = require('../../models/listing');
 
@@ -30,6 +31,41 @@ router.get('/view/:id', (req, res, next) => {
 
 router.get('/add', (req, res, next) => {
   res.render('listings/add');
+});
+
+router.post('/add', (req, res, next) => {
+  const { title, videoUrl, imageUrl, artist, year, duration, location, genre } = req.body;
+
+  const data = { title, videoUrl, imageUrl, artist, year, duration, location, genre };
+
+  const today = new Date();
+
+  if (!title || !videoUrl || !artist || !genre) {
+    req.flash('error', 'Please fill out all required fields');
+    return res.redirect('/listings/add');
+  }
+
+  if (!validator.isURL(videoUrl)) {
+    req.flash('error', 'Please check the video URL');
+    return res.redirect('/listings/add');
+  }
+
+  if (imageUrl && !validator.isURL(imageUrl)) {
+    req.flash('error', 'Please check the image URL');
+    return res.redirect('/listings/add');
+  }
+
+  if (year && (year < 1900 || year > today.getFullYear())) {
+    req.flash('error', 'Please check the year');
+    return res.redirect('/listings/add');
+  }
+
+  const newListing = new Listing(data);
+  newListing.save()
+    .then(listing => {
+      res.redirect('/listings/view/' + listing._id);
+    })
+    .catch(next);
 });
 
 module.exports = router;

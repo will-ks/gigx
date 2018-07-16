0a4ea8b62105787c28a15b54cbd56963df4c0bf1
Will Schaller <willschaller@gmail.com>
2018-07-16 12:55:53 +0200

    Make logout link in navbar work

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,31 +1,44 @@
 'use strict';
-const middle = document.getElementById('middle-bun');
-const icon = document.getElementById('complete-icon');
-const menu = document.querySelector('.menu-list');
 
-const deleteMiddle = function () {
-  middle.classList.toggle('hidden-bun');
-  updateTransition();
-  menuTransition();
-};
+function main () {
+  // --- Burger Menu --- //
+  const middle = document.getElementById('middle-bun');
+  const icon = document.getElementById('complete-icon');
+  const menu = document.querySelector('.menu-list');
 
-icon.addEventListener('click', deleteMiddle);
+  const deleteMiddle = function () {
+    middle.classList.toggle('hidden-bun');
+    updateTransition();
+    menuTransition();
+  };
 
-function updateTransition () {
-  const top = document.querySelector('div.top-burguer');
-  const bottom = document.querySelector('div.bottom-burguer');
-  top.classList.toggle('rotate-top');
-  bottom.classList.toggle('rotate-bottom');
+  icon.addEventListener('click', deleteMiddle);
+
+  function updateTransition () {
+    const top = document.querySelector('div.top-burguer');
+    const bottom = document.querySelector('div.bottom-burguer');
+    top.classList.toggle('rotate-top');
+    bottom.classList.toggle('rotate-bottom');
+  }
+
+  function displayMenu () {
+    menu.classList.toggle('menu-hidden');
+  }
+
+  icon.addEventListener('click', displayMenu);
+
+  function menuTransition () {
+    menu.classList.toggle('slide-menu');
+  }
+
+  icon.addEventListener('click', menuTransition);
+
+  // --- Desktop Menu --- //
+
+  document.getElementById('logout-link').onclick = () => {
+    document.getElementById('logout-form').submit();
+    return false;
+  };
 }
 
-function displayMenu () {
-  menu.classList.toggle('menu-hidden');
-}
-
-icon.addEventListener('click', displayMenu);
-
-function menuTransition () {
-  menu.classList.toggle('slide-menu');
-}
-
-icon.addEventListener('click', menuTransition);
+window.onload = main;

81e61b452b1f575061005878746045e769838e34
Will Schaller <willschaller@gmail.com>
2018-07-16 12:40:18 +0200

    Change navbar links depending on logged in status. Redirect from home to listings if logged in

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
+  if (req.session.currentUser) {
+    return res.redirect('/listings');
+  }
   res.render('index', { title: 'Express' });
 });
 

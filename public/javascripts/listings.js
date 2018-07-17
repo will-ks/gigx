ffb37f77085a8895a892341acb1939764ca8640c
Javier Romero <romeropaolini@gmail.com>
2018-07-17 10:46:43 +0200

    Add styling to Add Page form

---
+++ public/javascripts/listings.js
@@ -0,0 +1,11 @@
+// function main() {
+//   const cards = document.querySelectorAll('.add-star');
+//   cards.
+
+//   .addEventListener('click', () => {
+// axios.post('/users/profile/stars/add/')
+//   });
+//   console.log("Holaaaa");
+// }
+
+// window.unload = main;

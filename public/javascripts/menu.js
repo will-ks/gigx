fb6042e1a42ddc70a700f0502ced03847c0dde1b
Will Schaller <willschaller@gmail.com>
2018-07-19 17:12:39 +0200

    Remove burger menu

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,53 +1,20 @@
 'use strict';
 
-function main () {
-  // --- Burger Menu --- //
-  const middle = document.getElementById('middle-bun');
-  const icon = document.getElementById('complete-icon');
-  const menu = document.querySelector('.menu-list');
+// --- Desktop Menu --- //
 
-  const deleteMiddle = function () {
-    middle.classList.toggle('hidden-bun');
-    updateTransition();
-    menuTransition();
-  };
+const profileMenu = document.querySelector('.dropdown-menu');
+const profileIcon = document.getElementById('profile-icon');
 
-  icon.addEventListener('click', deleteMiddle);
+function profileMenuTransition() {
+  profileMenu.classList.toggle('not-shown');
+}
 
-  function updateTransition () {
-    const top = document.querySelector('div.top-burguer');
-    const bottom = document.querySelector('div.bottom-burguer');
-    top.classList.toggle('rotate-top');
-    bottom.classList.toggle('rotate-bottom');
-  }
+profileIcon.addEventListener('click', profileMenuTransition);
 
-  function displayMenu () {
-    menu.classList.toggle('menu-hidden');
-  }
-
-  icon.addEventListener('click', displayMenu);
-
-  function menuTransition () {
-    menu.classList.toggle('slide-menu');
-  }
-
-  icon.addEventListener('click', menuTransition);
-
-  // --- Desktop Menu --- //
-
-  const profileMenu = document.querySelector('.dropdown-menu');
-  const profileIcon = document.getElementById('profile-icon');
-
-  function profileMenuTransition () {
-    profileMenu.classList.toggle('not-shown');
-  }
-
-  profileIcon.addEventListener('click', profileMenuTransition);
-
-  document.getElementById('logout-link').onclick = () => {
-    document.getElementById('logout-form').submit();
-    return false;
-  };
+document.getElementById('logout-link').onclick = () => {
+  document.getElementById('logout-form').submit();
+  return false;
+};
 }
 
 window.onload = main;
